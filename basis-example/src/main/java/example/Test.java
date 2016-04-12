af01db04674028536277ae98b80496a88b7277f7
lindechang <504828318@qq.com>
2016-04-12 11:29:06 +0800

    test

---
+++ basis-example/src/main/java/example/Test.java
@@ -0,0 +1,10 @@
+package example;
+
+/**
+ * Created by lindec on 2016/4/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("dsfdsaf");
+    }
+}
